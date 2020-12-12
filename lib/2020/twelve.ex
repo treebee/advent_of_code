0a15584aca8018afbdc8febb9441adf221fa1d7e
Patrick Muehlbauer <patrick@opentrash.org>
2020-12-12 10:59:01 +0100

    simplyfy

--- lib/2020/twelve.ex
+++ lib/2020/twelve.ex
@@ -1,6 +1,6 @@
 defmodule AdventOfCode.TwentyTwenty.Twelve do
   use AdventOfCode
-  @opposite_dirs %{"E" => "W", "W" => "E", "N" => "S", "S" => "N"}
+
   def solve(filename \\ "twelve.txt") do
     instructions =
       filename
@@ -11,71 +11,58 @@ defmodule AdventOfCode.TwentyTwenty.Twelve do
     {instructions |> navigate(), instructions |> navigate2()}
   end
 
-  def rotate("E", "R", 90), do: "S"
-  def rotate("S", "R", 90), do: "W"
-  def rotate("W", "R", 90), do: "N"
-  def rotate("N", "R", 90), do: "E"
-  def rotate("E", "L", 90), do: "N"
-  def rotate("S", "L", 90), do: "E"
-  def rotate("W", "L", 90), do: "S"
-  def rotate("N", "L", 90), do: "W"
-  def rotate("E", "R", 270), do: "N"
-  def rotate("S", "R", 270), do: "E"
-  def rotate("W", "R", 270), do: "S"
-  def rotate("N", "R", 270), do: "W"
-  def rotate("E", "L", 270), do: "S"
-  def rotate("S", "L", 270), do: "W"
-  def rotate("W", "L", 270), do: "N"
-  def rotate("N", "L", 270), do: "E"
-  def rotate(face, _, 180), do: Map.get(@opposite_dirs, face)
+  def navigate(instructions), do: navigate(instructions, {0, 1}, {0, 0})
 
-  def navigate(instructions), do: navigate(instructions, "E", %{})
-
-  def navigate([["F", n] | tail], facing, acc),
-    do: navigate(tail, facing, Map.put(acc, facing, Map.get(acc, facing, 0) + n))
-
-  def navigate([["R", n] | tail], "E", acc), do: navigate(tail, rotate("E", "R", n), acc)
-  def navigate([["L", n] | tail], "E", acc), do: navigate(tail, rotate("E", "L", n), acc)
-
-  def navigate([[direction, n] | tail], facing, acc),
-    do: navigate(tail, facing, Map.put(acc, direction, Map.get(acc, direction, 0) + n))
-
-  def navigate([], _, acc) do
-    abs(Map.get(acc, "N", 0) - Map.get(acc, "S", 0)) +
-      abs(Map.get(acc, "E", 0) - Map.get(acc, "W", 0))
+  def navigate([["F", n] | tail], {dx, dy}, {x, y}) do
+    navigate(tail, {dx, dy}, {x + dx * n, y + dy * n})
   end
 
-  def move([], _, _, acc), do: acc
+  def navigate([["N", n] | tail], direction, {x, y}),
+    do: navigate(tail, direction, {x + n, y})
 
-  def move([direction | tail], waypoint, n, acc) do
-    move(
-      tail,
-      waypoint,
-      n,
-      Map.put(acc, direction, Map.get(acc, direction, 0) + n * Map.get(waypoint, direction))
-    )
+  def navigate([["S", n] | tail], direction, {x, y}),
+    do: navigate(tail, direction, {x - n, y})
+
+  def navigate([["E", n] | tail], direction, {x, y}),
+    do: navigate(tail, direction, {x, y + n})
+
+  def navigate([["W", n] | tail], direction, {x, y}),
+    do: navigate(tail, direction, {x, y - n})
+
+  def navigate([[action, n] | tail], {dx, dy}, pos) when {action, n} in [{"R", 90}, {"L", 270}],
+    do: navigate(tail, {-1 * dy, dx}, pos)
+
+  def navigate([[action, n] | tail], {dx, dy}, pos) when {action, n} in [{"L", 90}, {"R", 270}],
+    do: navigate(tail, {dy, -1 * dx}, pos)
+
+  def navigate([[action, 180] | tail], {dx, dy}, pos) when action in ["R", "L"] do
+    navigate(tail, {-1 * dx, -1 * dy}, pos)
   end
 
-  def navigate2(instructions), do: navigate2(instructions, %{"E" => 10, "N" => 1}, %{})
-
-  def navigate2([["F", n] | tail], waypoint, acc) do
-    acc = move(Map.keys(waypoint), waypoint, n, acc)
-    navigate2(tail, waypoint, acc)
+  def navigate([], _, {x, y}) do
+    abs(x) + abs(y)
   end
 
-  def navigate2([[action, n] | tail], waypoint, acc) when action in ["R", "L"] do
-    waypoint =
-      for {direction, value} <- waypoint, into: %{}, do: {rotate(direction, action, n), value}
+  def navigate2(instructions), do: navigate2(instructions, {1, 10}, {0, 0})
 
-    navigate2(tail, waypoint, acc)
-  end
+  def navigate2([["F", n] | tail], {dx, dy}, {x, y}),
+    do: navigate2(tail, {dx, dy}, {x + dx * n, y + dy * n})
 
-  def navigate2([[dir, n] | tail], waypoint, acc) do
-    waypoint = Map.put(waypoint, dir, Map.get(waypoint, dir, 0) + n)
-    navigate2(tail, waypoint, acc)
-  end
+  def navigate2([[action, n] | tail], {dx, dy}, pos) when {action, n} in [{"R", 90}, {"L", 270}],
+    do: navigate2(tail, {-1 * dy, dx}, pos)
 
-  def navigate2([], wp, acc) do
-    navigate([], wp, acc)
+  def navigate2([[action, n] | tail], {dx, dy}, pos) when {action, n} in [{"L", 90}, {"R", 270}],
+    do: navigate2(tail, {dy, -1 * dx}, pos)
+
+  def navigate2([[action, 180] | tail], {dx, dy}, pos) when action in ["R", "L"],
+    do: navigate2(tail, {-1 * dx, -1 * dy}, pos)
+
+  def navigate2([["N", n] | tail], {dx, dy}, {x, y}), do: navigate2(tail, {dx + n, dy}, {x, y})
+  def navigate2([["S", n] | tail], {dx, dy}, {x, y}), do: navigate2(tail, {dx - n, dy}, {x, y})
+  def navigate2([["E", n] | tail], {dx, dy}, {x, y}), do: navigate2(tail, {dx, dy + n}, {x, y})
+  def navigate2([["W", n] | tail], {dx, dy}, {x, y}), do: navigate2(tail, {dx, dy - n}, {x, y})
+
+  def navigate2([], _, {x, y}) do
+    abs(x) + abs(y)
   end
 end
